2db6bd34c00a502c83bb0a885993acb6fd35a0c9
DavidSDP <davidsahuquillodepablos@gmail.com>
2020-05-07 15:02:42 +0200

    más cosillas

---
+++ js-custom/publicar_moda.js
@@ -0,0 +1,39 @@
+var nombre, marca, tipo, precio, descripcion;
+
+function inicializar(){
+	nombre = document.getElementById("publicar_nombre");
+	marca = document.getElementById("publicar_marca");
+	tipo = document.getElementById("publicar_tipo");
+	precio = document.getElementById("publicar_precio");
+	descripcion = document.getElementById("publicar_descripcion");
+	//email = document.getElementById("publicar_email");
+}
+
+function publicar_moda(){
+	var URL = "data/moda.json";
+	var categoria = "moda";
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	        var json_moda = JSON.parse(this.responseText);
+	        json_moda.push({"name": nombre,
+	    					"descripcion" : descripcion,
+	    					"tipo" : tipo,
+	    					"identificador" : identificador,
+	    					"data-propios" : [
+	    						{
+	    							"marca": marca
+	    						}
+	    					]}
+	    					);
+        // we send with new request the updated JSON file to the server:
+        xhr.open("POST", URL, true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        // if you want to handle the POST response write (in this case you do not need it):
+        // xhr.onreadystatechange = function(){ /* handle POST response */ };
+        xhr.send("jsonTxt="+JSON.stringify(json_moda));
+        // but on this place you have to have a server for write updated JSON to the file
+	}
+	request.open('GET', requestURL, false);
+	request.send();
+}
