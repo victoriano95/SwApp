214d0259585ea9e70892f69b95a136fd62c37ba6
DavidSDP <davidsahuquillodepablos@gmail.com>
2020-05-08 12:19:24 +0200

    Update publicar_moda.php

--- data/publicar_moda.php
+++ data/publicar_moda.php
@@ -3,7 +3,7 @@
 	// Handling data in JSON format on the server-side using PHP
 	//
 	echo "AHJFBUSJEHB";
-	header("Content-Type: application/json");
+	//header("Content-Type: application/json");
 	// build a PHP variable from JSON sent using POST method
 	$v = json_decode(stripslashes(file_get_contents("php://input")));
 	// encode the PHP variable to JSON and send it back on client-side
