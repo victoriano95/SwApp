261064689e88973ebd7b9b3aa0bf9688c04ac680
DavidSDP <davidsahuquillodepablos@gmail.com>
2020-05-08 11:28:40 +0200

    más cosillas

--- js-custom/publicar_moda.js
+++ js-custom/publicar_moda.js
@@ -1,25 +1,12 @@
-var nombre, marca, tipo, precio, descripcion;
-
-function inicializar(){
-	nombre = document.getElementById("publicar_nombre");
-	marca = document.getElementById("publicar_marca");
-	tipo = document.getElementById("publicar_tipo");
-	precio = document.getElementById("publicar_precio");
-	descripcion = document.getElementById("publicar_descripcion");
-	//email = document.getElementById("publicar_email");
+function publicar_moda(){
+	var URL = "data/publicar_moda.php";
+	var request = new XMLHttpRequest();
+	var formElement = document.getElementById("nuevo_producto_form");
+	request.open("POST", URL);
+	request.send(new FormData(formElement));
 }
 
-function publicar_moda(){
-	        	alert("0");$.support.cors = true;
-	inicializar();
-	var URL = "data/moda.json";
-	var categoria = "moda";
-	var request = new XMLHttpRequest();
-	        	alert("1");
-	request.onreadystatechange = function() {
-	    if (this.readyState == 4 && this.status == 200) {
-	        	alert("2");
-	        var my_json = JSON.parse(this.responseText);
+/*var my_json = JSON.parse(this.responseText);
 	        var identificador = my_json[categoria].length;
 	        my_json[categoria].push({"name": nombre.value,
 	    					"descripcion" : descripcion.value,
@@ -30,44 +17,4 @@ function publicar_moda(){
 	    							"marca": marca.value
 	    						}
 	    					]}
-	    					);
-	        alert("3");
-		        console.log("Se procede a lanzar POST -> ");
-		        console.log(my_json);
-	        //request.open("POST", URL, true);
-	        //request.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-	        //request.send(my_json);
-
-
-
-			var URL2 = "data/moda2.json";
-			$.ajax({
-			type: "POST",
-			url: URL2,
-			data: my_json,
-			contentType: "application/json; charset=utf-8",
-			dataType: "json",
-			success: function(msg) {
-				alert("ok!");
-			 }
-			});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	    }
-    }
-	request.open('GET', URL, false);
-	request.send();
-}
+	    					);*/
