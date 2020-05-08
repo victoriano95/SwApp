afff8fc00ac04ddaba6c093bf2b310741f4b6872
DavidSDP <davidsahuquillodepablos@gmail.com>
2020-05-08 12:01:31 +0200

    más cosillas

---
+++ data/publicar_moda.php
@@ -0,0 +1,11 @@
+<?php
+
+	// Handling data in JSON format on the server-side using PHP
+	//
+	header("Content-Type: application/json");
+	// build a PHP variable from JSON sent using POST method
+	$v = json_decode(stripslashes(file_get_contents("php://input")));
+	// encode the PHP variable to JSON and send it back on client-side
+	echo "AHJFBUSJEHB";
+	echo json_encode($v);
+?>
--- js-custom/publicar_moda.js
+++ js-custom/publicar_moda.js
@@ -1,20 +1,30 @@
-function publicar_moda(){
-	var URL = "data/publicar_moda.php";
-	var request = new XMLHttpRequest();
-	var formElement = document.getElementById("nuevo_producto_form");
-	request.open("POST", URL);
-	request.send(new FormData(formElement));
+var nombre, marca, tipo, precio, descripcion;
+
+function inicializar(){
+	nombre = document.getElementById("publicar_nombre");
+	marca = document.getElementById("publicar_marca");
+	tipo = document.getElementById("publicar_tipo");
+	precio = document.getElementById("publicar_precio");
+	descripcion = document.getElementById("publicar_descripcion");
+	//email = document.getElementById("publicar_email");
 }
 
-/*var my_json = JSON.parse(this.responseText);
-	        var identificador = my_json[categoria].length;
-	        my_json[categoria].push({"name": nombre.value,
-	    					"descripcion" : descripcion.value,
-	    					"tipo" : tipo.value,
-	    					"identificador" : identificador,
-	    					"data-propios" : [
-	    						{
-	    							"marca": marca.value
-	    						}
-	    					]}
-	    					);*/
+function publicar_moda(){
+	inicializar();
+	var request = new XMLHttpRequest();
+	var categoria = "moda";
+    var my_json = {categoria: []};
+    my_json[categoria].push({"name": nombre.value,
+		"descripcion" : descripcion.value,
+		"tipo" : tipo.value,
+		"data-propios" : [
+			{
+				"marca": marca.value
+			}
+		]}
+		);
+    console.log(my_json);
+    request.open("POST", "data/publicar_moda.php", true);
+    request.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    request.send(JSON.stringify(my_json));
+}
