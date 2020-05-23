30438d139db25f94671295cdf6a792abfa0fed00
victoriano95 <58981102+victoriano95@users.noreply.github.com>
2020-05-23 16:06:19 +0200

    Update loader_productos.js

--- js-custom/loader_productos.js
+++ js-custom/loader_productos.js
@@ -7,8 +7,8 @@ for(var i = 0; i<URLs.length ; i++){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var div = document.getElementById("lista_productos");
-            div.innerHTML = "";
+            //var div = document.getElementById("lista_productos");
+            //div.innerHTML = "";
             var jsonfile = JSON.parse(this.responseText);
             for(item in jsonfile[categories[i]]){
                 console.log(categories[i]);
